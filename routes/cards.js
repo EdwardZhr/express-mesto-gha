6aaf881cd7460fcdbc269ac2fb0338ca11800920
Ed <zhrebec_edvard@mail.ru>
2023-03-30 07:31:01 +0300

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -17,7 +17,15 @@ router.delete('/:cardId', celebrate({
     cardId: Joi.string().alphanum().length(24),
   }),
 }), deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
