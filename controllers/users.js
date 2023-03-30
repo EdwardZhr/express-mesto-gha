5cbfea4750c2ba1c66f9f7608e203acbd951a730
Ed <zhrebec_edvard@mail.ru>
2023-03-30 06:48:06 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -37,7 +37,11 @@ const createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({
+      data: {
+        name: user.name, about: user.about, avatar: user.avatar, email: user.email,
+      },
+    }))
     .catch((err) => {
       if (err.code === 11000) {
         next(new ConflictError('Пользователь с таким email уже существует'));
